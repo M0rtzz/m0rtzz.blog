fe9cc4c1a8b1dc14c2a718637fd759c2d4b2311e
M0rtzz <147259692+M0rtzz@users.noreply.github.com>
2024-05-26 18:07:08 +0800

    🍱 asset: add svg

--- src/components/blocks/skills/index.tsx
+++ src/components/blocks/skills/index.tsx
@@ -9,24 +9,34 @@ import cmakeSvg from '@/images/logo/cmake.svg'
 import cppSvg from '@/images/logo/cpp.svg'
 import cSvg from '@/images/logo/c.svg'
 import dockerSvg from '@/images/logo/docker.svg'
+import excelSvg from '@/images/logo/excel.svg'
 import gitSvg from '@/images/logo/git.svg'
 import githubSvg from '@/images/logo/github.svg'
 import hppSvg from '@/images/logo/hpp.svg'
 import ideaSvg from '@/images/logo/idea.svg'
 import javaSvg from '@/images/logo/java.svg'
 import jsonSvg from '@/images/logo/json.svg'
+import latexSvg from '@/images/logo/latex.svg'
 import linuxSvg from '@/images/logo/linux.svg'
+import makefileSvg from '@/images/logo/makefile.svg'
+import markdownSvg from '@/images/logo/markdown.svg'
 import nginxSvg from '@/images/logo/nginx.svg'
 import nodejsSvg from '@/images/logo/nodejs.svg'
 import neovimSvg from '@/images/logo/neovim.svg'
 import nvmSvg from '@/images/logo/nvm.svg'
+import pdfSvg from '@/images/logo/pdf.svg'
+import pptSvg from '@/images/logo/ppt.svg'
+import prSvg from '@/images/logo/pr.svg'
+import psSvg from '@/images/logo/ps.svg'
 import pycharmSvg from '@/images/logo/pycharm.svg'
 import pythonSvg from '@/images/logo/python.svg'
+import pytorchSvg from '@/images/logo/pytorch.svg'
 import rosSvg from '@/images/logo/ros.svg'
 import shellSvg from '@/images/logo/shell.svg'
 import ubuntuSvg from '@/images/logo/ubuntu.svg'
 import vimSvg from '@/images/logo/vim.svg'
 import vscodeSvg from '@/images/logo/vscode.svg'
+import wordSvg from '@/images/logo/word.svg'
 import xmlSvg from '@/images/logo/xml.svg'
 import yamlSvg from '@/images/logo/yaml.svg'
 import zshSvg from '@/images/logo/zsh.svg'
@@ -46,24 +56,34 @@ const images: StaticImageData[] = [
   cppSvg,
   cSvg,
   dockerSvg,
+  excelSvg,
   gitSvg,
   githubSvg,
   hppSvg,
   ideaSvg,
   javaSvg,
   jsonSvg,
+  latexSvg,
   linuxSvg,
+  makefileSvg,
+  markdownSvg,
   nginxSvg,
   nodejsSvg,
   neovimSvg,
   nvmSvg,
+  pdfSvg,
+  pptSvg,
+  prSvg,
+  psSvg,
   pycharmSvg,
   pythonSvg,
+  pytorchSvg,
   rosSvg,
   shellSvg,
   ubuntuSvg,
   vimSvg,
   vscodeSvg,
+  wordSvg,
   xmlSvg,
   yamlSvg,
   zshSvg,
