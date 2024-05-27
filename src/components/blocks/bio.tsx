3ba01518ae35cb7c0a4aac2aaee6e7fcb8d8e773
M0rtzz <147259692+M0rtzz@users.noreply.github.com>
2024-05-27 11:52:24 +0800

    🐛 fix: increase the area of profile blocks

--- src/components/blocks/bio.tsx
+++ src/components/blocks/bio.tsx
@@ -12,7 +12,8 @@ export async function Bio() {
   return (
     <Block
       data-type='about'
-      className='prose z-10 col-span-2 row-span-2 max-w-full bg-gradient-to-br from-white to-amber-50 dark:prose-invert prose-h1:mb-0 dark:from-surface-1 dark:to-white/5 sm:max-lg:prose-p:my-1.5 md:col-span-4 md:row-span-1 md:max-xl:prose-p:my-2 lg:col-span-2 lg:row-span-1'
+      // className='prose z-10 col-span-2 row-span-2 max-w-full bg-gradient-to-br from-white to-amber-50 dark:prose-invert prose-h1:mb-0 dark:from-surface-1 dark:to-white/5 sm:max-lg:prose-p:my-1.5 md:col-span-4 md:row-span-1 md:max-xl:prose-p:my-2 lg:col-span-2 lg:row-span-1'
+      className='prose z-10 col-span-3 row-span-2 max-w-full bg-gradient-to-br from-white to-amber-50 dark:prose-invert prose-h1:mb-0 dark:from-surface-1 dark:to-white/5 sm:max-lg:prose-p:my-1.5 md:col-span-4 md:row-span-1 md:max-xl:prose-p:my-2 lg:col-span-2 lg:row-span-1'
     >
       <Markdown source={text} />
     </Block>
