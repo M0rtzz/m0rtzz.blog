9a77df41b1f57c3336b93690519c3b762a6dd446
M0rtzz <147259692+M0rtzz@users.noreply.github.com>
2024-05-28 19:57:28 +0800

    ✨ feat: add btn to copy the code in the block

--- src/markdown/components/pre.tsx
+++ src/markdown/components/pre.tsx
@@ -1,38 +1,38 @@
-'use client';
+'use client'
 
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef } from 'react'
 
 type PreProps = React.DetailedHTMLProps<
   React.HTMLAttributes<HTMLPreElement>,
   HTMLPreElement
->;
+>
 
 export const Pre = (props: PreProps) => {
-  const [isCopied, setIsCopied] = useState(false);
-  const preRef = useRef<HTMLPreElement>(null);
+  const [isCopied, setIsCopied] = useState(false)
+  const preRef = useRef<HTMLPreElement>(null)
 
   const handleCopy = async () => {
     if (preRef.current) {
-      const code = preRef.current.innerText;
+      const code = preRef.current.innerText
       try {
-        await navigator.clipboard.writeText(code);
-        setIsCopied(true);
-        setTimeout(() => setIsCopied(false), 2000);
+        await navigator.clipboard.writeText(code)
+        setIsCopied(true)
+        setTimeout(() => setIsCopied(false), 2000)
       } catch (error) {
-        console.error('Error copying code:', error);
+        console.error('Error copying code:', error)
       }
     }
-  };
+  }
 
   return (
-    <div className="relative">
+    <div className='relative'>
       <pre ref={preRef} {...props} />
       <button
         onClick={handleCopy}
-        className="absolute top-2 right-1 bg-gray-800 from-surface-1 justify-center bg-gradient-to-b to-white dark:to-white/5 py-1 px-2 rounded "
+        className='absolute top-2 right-1 bg-gray-800 from-surface-1 justify-center bg-gradient-to-b to-white dark:to-white/5 py-1 px-2 rounded'
       >
         {isCopied ? 'Copied!' : 'Copy'}
       </button>
     </div>
-  );
-};
+  )
+}
