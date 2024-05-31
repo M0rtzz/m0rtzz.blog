acc263b9235c8881f952fad535695e3c26880f64
M0rtzz <147259692+M0rtzz@users.noreply.github.com>
2024-05-31 10:24:35 +0800

    💄 ui: enlarge bilibili

--- src/components/blocks/bilibili.tsx
+++ src/components/blocks/bilibili.tsx
@@ -7,8 +7,8 @@ export const BiliBili = () => (
     className='group flex items-center justify-center bg-gradient-to-b from-blue-200 to-white text-black dark:from-blue-300/80 dark:to-white/70'
   >
     <svg
-      width='14.5em'
-      height='14.5em'
+      width='14.8em'
+      height='14.8em'
       viewBox='0 0 8000 8000'
       preserveAspectRatio='xMidYMid meet'
     >
