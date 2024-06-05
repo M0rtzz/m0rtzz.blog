1a44c577581c4ae438bd8df83227f8d715ab2dd5
M0rtzz <m0rtzz@outlook.com>
2024-06-05 19:38:17 +0800

    🔨 script(transfer.sh): add logic for handling errors

--- transfer.sh
+++ transfer.sh
@@ -1,11 +1,20 @@
 #!/bin/zsh
 
+function onCtrlC() {
+    exit 1
+}
+
+trap 'onCtrlC' INT
+
 echo $'\e[1;32m开始构建静态资源...\e[0m'
 
 rm -rf out/ .next/
 source /home/m0rtzz/.nvm/nvm.sh
 nvm use v18.20.3
-pnpm build
+pnpm build || {
+    echo $'\e[1;31m静态资源构建失败\e[0m'
+    exit 1
+}
 
 echo $'\e[1;32m静态资源构建完成\e[0m'
 
