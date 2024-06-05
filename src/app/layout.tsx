b08478dc243c1ce03c01e47d0039fd0375e26245
M0rtzz <m0rtzz@outlook.com>
2024-06-05 17:55:03 +0800

    ✨ feat: add logic for footer display

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
-import Image from 'next/image'
-
 import { clsx } from 'clsx'
 import { DarkToggleProvider, DarkToggleScript } from 'dark-toggle/react'
 import { Nunito, Handlee, Sorts_Mill_Goudy } from 'next/font/google'
 
 import { Provider } from '@/provider'
+
+import Footer from './footer'
 
 import type { Metadata, Viewport } from 'next'
 
@@ -113,43 +113,7 @@ export default function RootLayout({ children }) {
         <Provider>
           <DarkToggleProvider>{children}</DarkToggleProvider>
         </Provider>
-        <footer className='footer'>
-          <div className='container mx-auto flex items-center justify-center py-8 text-center text-xs'>
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-              <Image
-                src='/icon/police.png'
-                alt='Police Logo'
-                width={15}
-                height={15}
-                style={{ marginTop: '-2px', marginRight: '5px' }}
-              />
-              <a
-                href='https://beian.mps.gov.cn/#/query/webSearch'
-                target='_blank'
-                style={{ color: '#1E90FF', textDecoration: 'none' }}
-              >
-                公安备案号待审核
-              </a>
-            </div>
-            <div style={{ margin: '0 10px' }}></div>
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-              <Image
-                src='/icon/icp.png'
-                alt='ICP Logo'
-                width={18}
-                height={18}
-                style={{ marginTop: '-1.8px', marginRight: '5px' }}
-              />
-              <a
-                href='https://beian.miit.gov.cn'
-                target='_blank'
-                style={{ color: '#1E90FF', textDecoration: 'none' }}
-              >
-                豫ICP备2024072448号
-              </a>
-            </div>
-          </div>
-        </footer>
+        <Footer />
       </body>
     </html>
   )
