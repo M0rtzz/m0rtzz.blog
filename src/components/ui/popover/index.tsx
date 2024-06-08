e02e448d724fc06afe58ef45c0d3eb49143a755a
M0rtzz <m0rtzz@outlook.com>
2024-06-08 14:40:17 +0800

    🚧 wip: work in progress

--- src/components/ui/popover/index.tsx
+++ src/components/ui/popover/index.tsx
@@ -1,6 +1,17 @@
-import { Popover as unStyledPopover } from 'react-aria-components'
+import {
+  Popover as unStyledPopover,
+  Tooltip as unStyledTooltip,
+} from 'react-aria-components'
 import { withTw } from 'tw-styled'
 
-export const Popover = withTw(
-  unStyledPopover,
-)`data-[entering]:animate-in data-[entering]:fade-in data-[entering]:zoom-in-95 data-[entering]:slide-in-from-top-2 data-[exiting]:animate-out data-[exiting]:fade-out data-[exiting]:zoom-out-95`
+const cls =
+  'data-[entering]:animate-in data-[entering]:fade-in data-[entering]:zoom-in-95 data-[entering]:slide-in-from-top-2 data-[exiting]:animate-out data-[exiting]:fade-out data-[exiting]:zoom-out-95'
+
+// TODO fix tw-styled type error
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+export const Popover = withTw(unStyledPopover)(cls)
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+export const Tooltip = withTw(unStyledTooltip)(cls)
