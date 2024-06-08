82dddbcdc2aa9c99f6731f59f372f6b009059bb0
M0rtzz <m0rtzz@outlook.com>
2024-06-08 20:10:21 +0800

    💄 ui: change the location of directory

--- src/app/(article)/posts/[id]/page.tsx
+++ src/app/(article)/posts/[id]/page.tsx
@@ -126,7 +126,7 @@ export default async function Page({ params }: PageProps) {
           />
           <GiscusScript number={number} repo={`${repoOwner}/${repoName}`} />
         </article>
-        <aside className='sticky top-32 ml-auto h-fit w-[22ch] max-xl:hidden'>
+        <aside className='sticky-table sticky top-20 ml-auto h-fit w-[22ch] max-xl:hidden'>
           <h2 className='mb-4 whitespace-nowrap text-lg font-semibold tracking-wider has-[+ul:empty]:hidden'>
             TABLE OF CONTENTS
           </h2>
