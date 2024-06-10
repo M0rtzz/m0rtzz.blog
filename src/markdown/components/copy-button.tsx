52ffeaf81b553c30621d791f0adb9f44c315d9a0
M0rtzz <m0rtzz@outlook.com>
2024-06-10 15:25:21 +0800

    ♻️ refactor(copy-button.tsx): change logic of copying code

--- src/markdown/components/copy-button.tsx
+++ src/markdown/components/copy-button.tsx
@@ -1,7 +1,6 @@
 'use client'
 
-import { useState } from 'react'
-
+import React, { useState } from 'react'
 import { IconCheck, IconCopy } from '@tabler/icons-react'
 import { clsx } from 'clsx'
 
@@ -10,29 +9,66 @@ interface CopyButtonProps {
 }
 
 export const CopyButton = (props: CopyButtonProps) => {
-  const { content } = props
-  const [copied, setCopied] = useState(false)
+  const [isCopied, setIsCopied] = useState(false)
 
-  const handleClick = async () => {
-    // 移除结尾空行
-    const trimmedContent = content.trim()
-    await navigator.clipboard.writeText(trimmedContent)
-    setCopied(true)
-    setTimeout(() => setCopied(false), 1000)
+  const handleCopy = async () => {
+    let code = props.content // 使用 props.content 而不是从 DOM 获取
+    if (code) {
+      code = code.trimEnd()
+      // 尝试使用 navigator.clipboard API
+      if (navigator.clipboard) {
+        try {
+          await navigator.clipboard.writeText(code)
+          setIsCopied(true)
+          setTimeout(() => setIsCopied(false), 2000)
+        } catch (error) {
+          console.error('Error copying code with navigator.clipboard:', error)
+          // 如果 navigator.clipboard 失败，尝试使用 document.execCommand
+          fallbackCopyTextToClipboard(code)
+        }
+      } else {
+        // 如果 navigator.clipboard 不存在，直接使用 document.execCommand
+        fallbackCopyTextToClipboard(code)
+      }
+    }
   }
-  const Icon = copied ? IconCheck : IconCopy
+
+  // 此API目前已弃用，将来可能会无法使用
+  const fallbackCopyTextToClipboard = (text: string) => {
+    const textArea = document.createElement('textarea')
+    textArea.value = text
+    textArea.style.position = 'fixed' // 防止出现滚动条
+    document.body.appendChild(textArea)
+    textArea.focus()
+    textArea.select()
+    try {
+      // Deprecated
+      const successful = document.execCommand('copy')
+      if (successful) {
+        setIsCopied(true)
+        setTimeout(() => setIsCopied(false), 2000)
+      } else {
+        console.error('Fallback: Copying text command was unsuccessful')
+      }
+    } catch (err) {
+      console.error('Fallback: Oops, unable to copy', err)
+    }
+    document.body.removeChild(textArea)
+  }
+
+  const Icon = isCopied ? IconCheck : IconCopy
 
   return (
     <button
       className={clsx(
         'copy-button absolute -right-0 top-2 hidden rounded bg-gradient-to-b to-white p-2 hover:bg-color-1/10 group-hover:block dark:to-white/5',
-        copied
+        isCopied
           ? '!block bg-gray-800 bg-gradient-to-b from-surface-1 to-white text-green-500 '
           : 'text-gray-500',
       )}
-      onClick={handleClick}
+      onClick={handleCopy}
     >
-      {copied && (
+      {isCopied && (
         <span className='absolute-small absolute -left-2 top-1/2 -translate-x-full -translate-y-1/2 rounded bg-gray-800 bg-gradient-to-b from-surface-1 to-white px-2 py-1 text-gray-500 dark:to-white/5 '>
           Copied!
         </span>
