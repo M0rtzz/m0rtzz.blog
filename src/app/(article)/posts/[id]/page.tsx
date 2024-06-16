01ecb4c12f667ffe08db149024f41ecded15e86b
M0rtzz <m0rtzz@outlook.com>
2024-06-16 21:38:20 +0800

    💄 ui(page.tsx): widen table of contents

--- src/app/(article)/posts/[id]/page.tsx
+++ src/app/(article)/posts/[id]/page.tsx
@@ -125,7 +125,7 @@ export default async function Page({ params }: PageProps) {
         />
         <GiscusScript number={number} repo={`${repoOwner}/${repoName}`} />
       </article>
-      <aside className='sticky-table sticky top-20 ml-auto h-fit w-[22ch] max-xl:hidden'>
+      <aside className='sticky-table sticky top-20 ml-auto h-fit w-[24ch] max-xl:hidden'>
         <h2 className='mb-4 whitespace-nowrap text-lg font-semibold tracking-wider has-[+ul:empty]:hidden'>
           TABLE OF CONTENTS
         </h2>
