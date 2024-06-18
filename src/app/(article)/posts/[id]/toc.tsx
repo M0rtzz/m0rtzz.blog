f750d8f80198a9e2669eeaabd6a64df1941c8e62
M0rtzz <m0rtzz@outlook.com>
2024-06-18 13:54:44 +0800

    🚧 wip: wip

---
+++ src/app/(article)/posts/[id]/toc.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import { TOC } from 'react-markdown-toc/client'
+import { Collapsible, CollapsibleTrigger, CollapsibleContent } from '@/components/ui/collapsible'
+import { useRouter } from 'next/navigation';
+
+interface CustomTOCProps {
+  tocData: [any, Map<string, string>];
+}
+
+function CustomTOC({ tocData }: CustomTOCProps) {
+  const router = useRouter();
+
+  return (
+    <TOC
+      toc={tocData}
+      scrollAlign="center"
+      renderList={(children) => (
+        <CollapsibleContent className='pl-4 overflow-hidden data-[state=open]:animate-collapsible-down data-[state=closed]:animate-collapsible-up'>
+          {children}
+        </CollapsibleContent>
+      )}
+      renderListItem={(children, open) => (
+        <Collapsible open={open}>
+          {children}
+        </Collapsible>
+      )}
+      renderLink={(children, href, active) => (
+        <CollapsibleTrigger>
+          <span
+            data-active={active}
+            role="button"
+            onClick={() => {
+              router.push(href, { scroll: false });
+              const target = document.querySelector(href)
+              target?.scrollIntoView({ behavior: 'smooth' })
+            }}
+          >
+            {children}
+          </span>
+        </CollapsibleTrigger>
+      )}
+    />
+  );
+}
+
+export default CustomTOC
