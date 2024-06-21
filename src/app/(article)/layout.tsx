fbee7a0ceac12c669012ee8e1f04d24b3fb15e93
M0rtzz <m0rtzz@outlook.com>
2024-06-21 18:49:39 +0800

    💄 ui(layout.tsx): sticky header

--- src/app/(article)/layout.tsx
+++ src/app/(article)/layout.tsx
@@ -3,7 +3,7 @@ import { Header } from '@/components/header/article'
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Header />
+      <Header sticky />
       {children}
     </>
   )
