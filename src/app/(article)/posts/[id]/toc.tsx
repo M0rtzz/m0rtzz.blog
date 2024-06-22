be306bb4f7c855ef5ca8495b497b79383d7598ea
M0rtzz <m0rtzz@outlook.com>
2024-06-22 18:33:48 +0800

    ✅ test(toc.tsx): set scrollAlign to end

--- src/app/(article)/posts/[id]/toc.tsx
+++ src/app/(article)/posts/[id]/toc.tsx
@@ -15,7 +15,7 @@ export function TOCClient (props: TOCClientProps) {
   const router = useRouter()
   return (
     <TOC
-      scrollAlign='start'
+      scrollAlign='end'
       throttleTime={100}
       toc={toc}
       renderList={children => (
