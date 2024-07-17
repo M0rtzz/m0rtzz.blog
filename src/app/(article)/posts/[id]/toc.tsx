7bed942f426e9de409dac83cdd97424242c76892
M0rtzz <m0rtzz@outlook.com>
2024-07-17 13:27:48 +0800

    🎨 style(toc.tsx): just format code

--- src/app/(article)/posts/[id]/toc.tsx
+++ src/app/(article)/posts/[id]/toc.tsx
@@ -4,13 +4,17 @@ import { useRouter } from 'next/navigation'
 
 import { TOC, type TOCProps } from 'react-markdown-toc/client'
 
-import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '@/components/ui/collapsible'
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from '@/components/ui/collapsible'
 
 interface TOCClientProps {
   toc: TOCProps['toc']
 }
 
-export function TOCClient (props: TOCClientProps) {
+export function TOCClient(props: TOCClientProps) {
   const { toc } = props
   const router = useRouter()
   return (
@@ -19,11 +23,13 @@ export function TOCClient (props: TOCClientProps) {
       throttleTime={100}
       toc={toc}
       renderList={children => (
-        <CollapsibleContent className='pl-4 overflow-hidden data-[state=open]:animate-collapsible-down data-[state=closed]:animate-collapsible-up'>
+        <CollapsibleContent className='overflow-hidden pl-4 data-[state=closed]:animate-collapsible-up data-[state=open]:animate-collapsible-down'>
           {children}
         </CollapsibleContent>
       )}
-      renderListItem={(children, open) => <Collapsible open={open}>{children}</Collapsible>}
+      renderListItem={(children, open) => (
+        <Collapsible open={open}>{children}</Collapsible>
+      )}
       renderLink={(children, href, active) => (
         <CollapsibleTrigger>
           <span
