b2abf9a4a6df0cd4ca5ac570d41e5aaf04c1c717
M0rtzz <m0rtzz@outlook.com>
2024-07-19 10:01:59 +0800

    🐛 fix(music.tsx): fix the bug of the music not automatically playing again when it ends

--- src/components/header/article/music.tsx
+++ src/components/header/article/music.tsx
@@ -5,13 +5,12 @@ import React, { useState, useRef } from 'react'
 import { IconMusic, IconMusicOff } from '@tabler/icons-react'
 
 export const MusicToggle = () => {
-  const audioRef = useRef<HTMLAudioElement | null>(null) // 指定 useRef 类型为 HTMLAudioElement 或 null
+  const audioRef = useRef<HTMLAudioElement | null>(null)
   const [isPlaying, setIsPlaying] = useState(false)
   const [isRotating, setIsRotating] = useState(false)
 
   const togglePlay = () => {
     if (audioRef.current) {
-      // 添加条件判断确保 audioRef.current 不为 null
       if (audioRef.current.paused) {
         audioRef.current.play()
         setIsPlaying(true)
@@ -24,9 +23,22 @@ export const MusicToggle = () => {
     }
   }
 
+  const handleMusicEnd = () => {
+    setIsPlaying(false)
+    setIsRotating(false)
+    setTimeout(() => {
+      if (audioRef.current) {
+        audioRef.current.currentTime = 0 // 重置播放时间到开头
+        audioRef.current.play() // 重新播放音乐
+        setIsPlaying(true)
+        setIsRotating(true)
+      }
+    }, 1000) // 停顿1秒
+  }
+
   return (
     <div>
-      <audio ref={audioRef} src='/music/music.mp3' />
+      <audio ref={audioRef} src='/music/music.mp3' onEnded={handleMusicEnd} />
       <button
         className={`rounded p-1.5 outline-none transition-colors hover:bg-surface-1 pressed:bg-surface-1 ${isRotating ? 'rotate-animation' : ''}`}
         aria-label={isPlaying ? 'Pause Music' : 'Play Music'}
