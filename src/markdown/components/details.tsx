3f035d1584203d094a883675786bebab1330ae3e
M0rtzz <m0rtzz@outlook.com>
2024-07-28 15:25:17 +0800

    🐛 fix(details.tsx): fix the details block display bug

--- src/markdown/components/details.tsx
+++ src/markdown/components/details.tsx
@@ -1,12 +1,13 @@
 interface DetailsProps {
-  summary?: string
   children: React.ReactNode
+  summary?: string
 }
 export const Details = (props: DetailsProps) => {
-  const { summary } = props
+  const { children, summary } = props
   return (
     <details className='mdx-components rounded-lg bg-surface-1 p-4'>
       <summary>{summary}</summary>
+      {children}
     </details>
   )
 }
