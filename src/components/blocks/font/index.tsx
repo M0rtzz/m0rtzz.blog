c363f7b4e2148d1d5295e784abacbe2118f9e220
M0rtzz <m0rtzz@outlook.com>
2024-10-19 14:00:26 +0800

    💄 ui: update the style of font-toggle block

--- src/components/blocks/font/index.tsx
+++ src/components/blocks/font/index.tsx
@@ -13,30 +13,38 @@ export const Font = () => {
       className='group grid grid-cols-3 grid-rows-4 items-center justify-items-center bg-surface-1 text-lg font-semibold text-color-1 lg:grid-cols-5 lg:grid-rows-6'
     >
       <AnimateSpan className='block'>Aa</AnimateSpan>
-      <AnimateSpan className='block'>a</AnimateSpan>
-      <AnimateSpan className='block'>A</AnimateSpan>
-      <AnimateSpan>Aa</AnimateSpan>
-      <AnimateSpan>a</AnimateSpan>
-      <AnimateSpan>a</AnimateSpan>
-      <AnimateSpan>Aa</AnimateSpan>
-      <AnimateSpan>a</AnimateSpan>
-      <AnimateSpan>A</AnimateSpan>
-      <AnimateSpan>a</AnimateSpan>
-      <AnimateSpan>Aa</AnimateSpan>
+      <AnimateSpan className='block'>Bb</AnimateSpan>
+      <AnimateSpan className='block'>Cc</AnimateSpan>
+      <AnimateSpan className='block'>Dd</AnimateSpan>
+      <AnimateSpan className='block'>Ee</AnimateSpan>
+      <AnimateSpan className='block'>Ff</AnimateSpan>
+      <AnimateSpan className='block'>Gg</AnimateSpan>
+      <AnimateSpan className='block'>Hh</AnimateSpan>
+      <AnimateSpan className='block'>Ii</AnimateSpan>
+      <AnimateSpan className='block'>Jj</AnimateSpan>
+      <AnimateSpan className='block'>Kk</AnimateSpan>
+      <AnimateSpan className='block'></AnimateSpan>
+      <AnimateSpan className='block'>Ll</AnimateSpan>
+      <AnimateSpan className='block'></AnimateSpan>
+      <AnimateSpan className='block'>Mm</AnimateSpan>
+      <AnimateSpan className='block'></AnimateSpan>
       <Toggle />
-      <AnimateSpan className='block'>Aa</AnimateSpan>
-      <AnimateSpan className='block'>a</AnimateSpan>
-      <AnimateSpan className='block'>A</AnimateSpan>
-      <AnimateSpan>Aa</AnimateSpan>
-      <AnimateSpan>a</AnimateSpan>
-      <AnimateSpan>a</AnimateSpan>
-      <AnimateSpan>Aa</AnimateSpan>
-      <AnimateSpan>a</AnimateSpan>
-      <AnimateSpan>A</AnimateSpan>
-      <AnimateSpan>a</AnimateSpan>
-      <AnimateSpan>Aa</AnimateSpan>
-      <AnimateSpan>a</AnimateSpan>
-      <AnimateSpan>A</AnimateSpan>
+      <AnimateSpan className='block'></AnimateSpan>
+      <AnimateSpan className='block'>Nn</AnimateSpan>
+      <AnimateSpan className='block'></AnimateSpan>
+      <AnimateSpan className='block'>Oo</AnimateSpan>
+      <AnimateSpan className='block'></AnimateSpan>
+      <AnimateSpan className='block'>Pp</AnimateSpan>
+      <AnimateSpan className='block'>Qq</AnimateSpan>
+      <AnimateSpan className='block'>Rr</AnimateSpan>
+      <AnimateSpan className='block'>Ss</AnimateSpan>
+      <AnimateSpan className='block'>Tt</AnimateSpan>
+      <AnimateSpan className='block'>Uu</AnimateSpan>
+      <AnimateSpan className='block'>Vv</AnimateSpan>
+      <AnimateSpan className='block'>Ww</AnimateSpan>
+      <AnimateSpan className='block'>Xx</AnimateSpan>
+      <AnimateSpan className='block'>Yy</AnimateSpan>
+      <AnimateSpan className='block'>Zz</AnimateSpan>
     </Block>
   )
 }
--- src/components/blocks/font/toggle.tsx
+++ src/components/blocks/font/toggle.tsx
@@ -14,9 +14,9 @@ export const Toggle = () => {
       tabIndex={0}
       aria-label='Font Toggle Label'
       onClick={() => onToggle()}
-      className='col-span-3 row-span-2 flex items-center gap-2 p-4 text-3xl'
+      className='col-span-3 row-span-2 flex items-center gap-2 p-4 text-1xl'
     >
-      <span className='font-sans'>T</span>
+      <span className='font-sans'>Sans</span>
       <Switch
         aria-label='Font Toggle Switch'
         onChange={() => onToggle()}
@@ -27,7 +27,7 @@ export const Toggle = () => {
         )}
         isSelected={font === 'serif'}
       />
-      <span className='font-serif'>T</span>
+      <span className='font-serif'>Serif</span>
     </div>
   )
 }
