0bda748a177d43dcc1be567119c14ef482477fcf
M0rtzz <m0rtzz@outlook.com>
2025-06-30 21:41:27 +0800

    💄 ui(CustomizeCursor.tsx): update css class

--- src/components/ui/cursor/CustomizeCursor.tsx
+++ src/components/ui/cursor/CustomizeCursor.tsx
@@ -12,7 +12,7 @@ export default function CursorInitializer() {
         try {
           // 默认光标
           setANICursorWithGroupElement(
-            ["body", "html", "img"],
+            ["body", "html", "img", "div.relative:nth-child(n)", "div.relative:nth-child(n) > div:nth-child(n) > a:nth-child(n) > h2:nth-child(n)", "div.relative:nth-child(n) > div:nth-child(n) > p:nth-child(n)"],
             "/cursor/ani/arrow.ani"
           );
 
@@ -22,28 +22,28 @@ export default function CursorInitializer() {
             "/cursor/ani/cross.ani"
           );
 
-          // 链接和可点击元素（
-          setANICursorWithGroupElement(
-            ["a", "button", "[onclick]", "summary", "details > summary"],
-            "/cursor/ani/link.ani"
-          );
-
           // 可拖动元素
           setANICursor(
             "[draggable='true']",
             "/cursor/ani/move.ani"
           );
-
+          
           // 禁用状态
           setANICursor(
             "[disabled]",
             "/cursor/ani/no.ani"
           );
-
+          
           // 文本元素
           setANICursorWithGroupElement(
-            ["p", "h1", "h2", "h3", "h4", "h5", "h6", "span", "li", "td", "th"],
+            ["p", "h1", "h2", "h3", "h4", "h5", "h6", "li", "td", "th"],
             "/cursor/ani/beam.ani"
+          );
+          
+          // 链接和可点击元素（
+          setANICursorWithGroupElement(
+            ["a", "button", "[onclick]", "summary", "details > summary", ".col-span-3", "label.relative", "a.group\\/note:nth-child(n) > p:nth-child(n)", "div.relative:nth-child(n) > a:nth-child(n) > span:nth-child(n)", "div.relative:nth-child(n) > div:nth-child(n) > p:nth-child(n) > a:nth-child(n)"],
+            "/cursor/ani/link.ani"
           );
 
         } catch (error) {
