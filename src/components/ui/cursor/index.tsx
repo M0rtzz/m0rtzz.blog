8a427ea082c2a9d6b653d67db34c8c5133a90fea
Zihang Xu <m0rtzz@outlook.com>
2026-02-17 12:22:58 +0800

    fix: apply custom link cursor to TOC items

--- src/components/ui/cursor/index.tsx
+++ src/components/ui/cursor/index.tsx
@@ -76,6 +76,7 @@ export default function CursorInitializer() {
         [
           `a${exclude_element}`,
           `button${exclude_element}`,
+          `.toc-left${exclude_element}`,
           `summary${exclude_element}`,
           `[onclick]${exclude_element}`,
           `.col-span-3${exclude_element}`,
