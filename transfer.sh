67526855afac16b72f7435e6fa1126ce7e29a58f
M0rtzz <zihang_xu@hust.edu.cn>
2026-08-27 09:49:14 +0800

    ⚡️ perf(transfer.sh): reduce deployment transfers with incremental synchronization
    - replace full ZIP uploads with checksum-based rsync.
    - delay file updates and deletions until synchronization completes.
    - retain the existing Nginx and GitLab restart sequence.

--- transfer.sh
+++ transfer.sh
@@ -48,8 +48,6 @@ pnpm build || {
 
 echo $'\e[1;32m静态资源构建完成\e[0m'
 
-zip -r -q out.zip out/
-
 remote_user="root"
 remote_host="${CLOUD}"
 remote_folder="/root/Web/m0rtzz.blog/"
@@ -60,28 +58,30 @@ echo $'\e[1;32m代码拉取完成\e[0m'
 
 echo $'\e[1;32m开始传输静态资源...\e[0m'
 
-rsync -av --progress out.zip "${remote_user}"@"${remote_host}":"${remote_folder}"
+rsync \
+    --archive \
+    --checksum \
+    --delete-delay \
+    --delay-updates \
+    --partial \
+    --info=stats2 \
+    out/ \
+    "${remote_user}"@"${remote_host}":"${remote_folder}out/"
 
 ssh "${remote_user}"@"${remote_host}" "lsof -i :80 -i :443 | awk 'NR!=1 {print \$2}' | xargs -r kill -9 && echo $'\e[1;32mkill process success\e[0m' || echo $'\e[1;31mkill process fail\e[0m'"
 
 ssh "${remote_user}"@"${remote_host}" "
-if pgrep -x nginx > /dev/null; then 
-    if nginx -s stop; then 
-        echo \$'\e[1;32mnginx -s stop success\e[0m'; 
-    else 
-        echo \$'\e[1;31mnginx -s stop fail\e[0m'; 
+if pgrep -x nginx > /dev/null; then
+    if nginx -s stop; then
+        echo \$'\e[1;32mnginx -s stop success\e[0m';
+    else
+        echo \$'\e[1;31mnginx -s stop fail\e[0m';
     fi
-else 
+else
     echo \$'\e[1;33mnginx is not running\e[0m';
 fi
 "
 
-ssh "${remote_user}"@"${remote_host}" "rm -rf ${remote_folder}out/ ${remote_folder}.next/"
-
-ssh "${remote_user}"@"${remote_host}" "unzip -o -q ${remote_folder}out.zip -d ${remote_folder} && rm -f ${remote_folder}out.zip && echo $'\e[1;32munzip success\e[0m' || echo $'\e[1;31munzip fail\e[0m'"
-
 ssh "${remote_user}"@"${remote_host}" "gitlab-ctl stop && nginx && nginx -s reload && gitlab-ctl start && echo $'\e[1;32mnginx -s reload success\e[0m' || echo $'\e[1;31mnginx -s reload fail\e[0m'"
-
-rm -f out.zip
 
 echo $'\e[1;32m部署完成\e[0m'
